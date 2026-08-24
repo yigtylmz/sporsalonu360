1d626d30444f668cd277a8da525fb1b7136d02be
yigtylmz <kocbabametal@gmail.com>
2026-08-24 22:07:30 +0300

    Marzipano tur

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-56",
+      "name": "56",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "yaw": -1.7383878083620417,
+        "pitch": 0.0646318387549698,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6303204042144372,
+          "pitch": 1.0521217850048608,
+          "rotation": 12.566370614359176,
+          "target": "1-23_marzipano"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-23_marzipano",
+      "name": "23_marzipano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.13210413634834062,
+        "pitch": 1.2104960893048098,
+        "fov": 1.3401592523986738
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3315513429152954,
+          "pitch": 0.7677806924946733,
+          "rotation": 0,
+          "target": "0-56"
+        },
+        {
+          "yaw": 1.5231895875523502,
+          "pitch": 1.1129091485586198,
+          "rotation": 0,
+          "target": "2-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-02",
+      "name": "02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2187001654556333,
+          "pitch": 1.1042478803724887,
+          "rotation": 0,
+          "target": "1-23_marzipano"
+        },
+        {
+          "yaw": 3.0492245123786645,
+          "pitch": 0.7679868337522144,
+          "rotation": 0,
+          "target": "3-55"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-55",
+      "name": "55",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 250,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4588909994243995,
+          "pitch": 0.933094459049034,
+          "rotation": 0,
+          "target": "3-55"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
